614fd45eacaff88dc96520afdad40b362d6e3712
Xatabch <ivan.morozov@list.ru>
2018-04-04 01:00:03 +0300

    add Road map

---
+++ main.py
@@ -0,0 +1,20 @@
+
+class InstagramParse:
+
+    def __init__(self, path):
+        pass
+
+    def get_connection(self):
+        pass
+
+
+    def get_first_twelve(self, user_array):
+        pass
+
+
+    def get_photos_from_user_page(self, user_path, photo_num):
+        pass
+
+
+    def get_user_information(self, user_path):
+        pass
