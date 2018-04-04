cf164f445f8d5d4da5de921b7836f610893268ce
Ivan <ivan.morozov@list.ru>
2018-04-04 12:46:00 +0300

    add init and requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
 
 class InstagramParse:
 
-    def __init__(self, path):
-        pass
+    def __init__(self, user_path):
+        self._user_path = user_path
+        response = requests.get(self._user_path, headers={'User-Agent': UserAgent().firefox})
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        for i,elem in enumerate(soup.body):
+            if (i == 11):
+                self._ProfilePageContainer ="https://www.instagram.com" + str(elem).split("src")[1].split('"')[1]
+                print(self._ProfilePageContainer)
 
     def get_connection(self):
         pass
@@ -18,3 +29,7 @@ class InstagramParse:
 
     def get_user_information(self, user_path):
         pass
+
+
+link = "https://www.instagram.com/cappub/"
+parse = InstagramParse(link)
